8cbb057a0c18bec015175cacc09e7e7fec05badc
Agustin-PerezR <72412854+Agustin-PerezR@users.noreply.github.com>
2021-10-28 18:32:40 -0300

    Add files via upload

---
+++ TpAPI/script.js
@@ -0,0 +1,60 @@
+function mostrarAlumno(){
+    $.ajax ({
+        url: "http://localhost:8080/api/alumnos/",
+        type: "GET",
+        data : {id: 102, nombre: "Carlos", edad: 18}
+    })
+    .done(function(data){
+        let saludo = data.mensaje;
+        var datos= JSON.stringify(data);
+        document.getElementById("mostrarAlumnos").innerHTML= datos;
+    })
+    .fail(function(jqXHR,textStatus, errorTrhow){
+        console.log("error, no se encontraron datos")
+        console.log(jqXHR)
+        console.log(textStatus)
+        console.log(errorTrhow)
+    })
+    
+
+    
+
+}function agregarAlumno(){
+    $.ajax ({
+        url: "http://localhost:8080/api/alumnos/",
+        type: "POST"
+    })
+    .done(function(response){
+        let saludo = response.mensaje;
+        var datos= JSON.stringify(response);
+        document.getElementById("agregarAlumno").innerHTML= datos;
+    })
+    .fail(function(jqXHR,textStatus, errorTrhow){
+        console.log("error, no se encontraron datos")
+        console.log(jqXHR)
+        console.log(textStatus)
+        console.log(errorTrhow)
+    })
+    
+
+    
+
+}
+function borrarAlumno(){
+    $.ajax( {
+        url : 'http://localhost:8080/api/alumnos/',
+        type : 'DELETE'
+    })
+        .done(function ( data ) {
+                let saludo = data.mensaje;
+                var datos= JSON.stringify(data);
+                document.getElementById("borrarAlumno").innerHTML= datos;    
+        })  
+        
+        .fail(function(jqXHR,textStatus, errorTrhow){
+            console.log("error, no se encontraron datos")
+            console.log(jqXHR)
+            console.log(textStatus)
+            console.log(errorTrhow)
+        })
+}
